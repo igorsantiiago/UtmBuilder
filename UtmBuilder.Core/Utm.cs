4e285d7b4c9bafb97e0682d58dbfcc21bf79d93a
Igor Santiago <contato.igorsantiago@outlook.com>
2023-06-20 16:47:37 -0300

    Extension methods and Implicit operators

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -1,4 +1,6 @@
+using UtmBuilder.Core.Extensions;
 using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core;
 
@@ -19,5 +21,44 @@ public class Utm
     /// Campain details
     /// </summary>
     public Campaign Campaign { get; }
+
+    public static implicit operator string(Utm utm)
+        => utm.ToString();
+
+    public static implicit operator Utm(string link)
+    {
+        if (string.IsNullOrEmpty(link))
+            throw new InvalidUrlException();
+
+        var url = new Url(link);
+        var segments = url.Address.Split("?");
+
+        if (segments.Length == 1)
+            throw new InvalidUrlException("No segments were provided");
+
+        var parms = segments[1].Split("&");
+        var source = parms.Where(x => x.StartsWith("utm_source")).FirstOrDefault("").Split("=")[1];
+        var medium = parms.Where(x => x.StartsWith("utm_medium")).FirstOrDefault("").Split("=")[1];
+        var name = parms.Where(x => x.StartsWith("utm_name")).FirstOrDefault("").Split("=")[1];
+        var id = parms.Where(x => x.StartsWith("utm_id")).FirstOrDefault("").Split("=")[1];
+        var term = parms.Where(x => x.StartsWith("utm_term")).FirstOrDefault("").Split("=")[1];
+        var content = parms.Where(x => x.StartsWith("utm_content")).FirstOrDefault("").Split("=")[1];
+
+        var utm = new Utm(new Url(segments[0]), new Campaign(source, medium, name, id, term, content));
+
+        return utm;
+    }
+
+    public override string ToString()
+    {
+        var urlSegments = new List<string>();
+        urlSegments.AddIfNotNull("utm_source", Campaign.Source);
+        urlSegments.AddIfNotNull("utm_medium", Campaign.Medium);
+        urlSegments.AddIfNotNull("utm_name", Campaign.Name);
+        urlSegments.AddIfNotNull("utm_id", Campaign.Id);
+        urlSegments.AddIfNotNull("utm_term", Campaign.Term);
+        urlSegments.AddIfNotNull("utm_content", Campaign.Content);
+        return $"{Url.Address}?{string.Join("&", urlSegments)}";
+    }
 
 }
--- UtmBuilder.Core/ValueObjects/Campaign.cs
+++ UtmBuilder.Core/ValueObjects/Campaign.cs
@@ -7,39 +7,39 @@ public class Campaign : ValueObject
     /// <summary>
     /// Generates a new campaing for a URL
     /// </summary>
-    /// <param name="name">Product, promo code, or slogan. One campaing name or id is required.</param>
     /// <param name="source">The referrer (e.g. google, newsletter).</param>
     /// <param name="medium">Marketing medium (e.g. cpc, banner, email).</param>
+    /// <param name="name">Product, promo code, or slogan. One campaing name or id is required.</param>
     /// <param name="id">The ads campaing id.</param>
     /// <param name="term">Identify the paid keywords.</param>
     /// <param name="content">Use to differentiate ads</param>
-    public Campaign(string name, string source, string medium,
+    public Campaign(string source, string medium, string name,
         string? id = null, string? term = null, string? content = null)
     {
-        Name = name;
         Source = source;
         Medium = medium;
+        Name = name;
         Id = id;
         Term = term;
         Content = content;
 
-        InvalidCampaignException.ThrowIfNull(name, "Name is invalid.");
         InvalidCampaignException.ThrowIfNull(source, "Source is invalid.");
         InvalidCampaignException.ThrowIfNull(medium, "Medium is invalid.");
+        InvalidCampaignException.ThrowIfNull(name, "Name is invalid.");
     }
 
     public string? Id { get; set; }
-    public string Name { get; set; }
 
     /// <summary>
     /// The referrer (e.g. google, newsletter).
     /// </summary>
     public string Source { get; set; }
-
     /// <summary>
     /// Marketing medium (e.g. cpc, banner, email).
     /// </summary>
     public string Medium { get; set; }
+
+    public string Name { get; set; }
 
     /// <summary>
     /// Identify the paid keywords.
